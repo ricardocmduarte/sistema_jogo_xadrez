40972e28f2894d0e4f726dc5f914e9e98b9348a0
Ricardo Duarte <ricnewline@gmail.com>
2020-07-30 10:23:46 -0300

    Frist class: Position

--- src/application/Programa.java
+++ src/application/Programa.java
@@ -1,10 +1,14 @@
 package application;
 
+import boardgame.Position;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello Word!");
+		Position pos = new Position(3, 5);
+		System.out.println(pos);
+				
 
 	}
 
