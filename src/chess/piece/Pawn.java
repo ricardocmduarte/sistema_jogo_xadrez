554275c9376f2eb69d406e2271e2e7bdc9e21a4a
Ricardo Duarte <ricnewline@gmail.com>
2020-08-30 18:40:18 -0300

    Bishop

--- src/chess/piece/Pawn.java
+++ src/chess/piece/Pawn.java
@@ -10,6 +10,11 @@ public class Pawn extends ChessPiece {
 	public Pawn(Board board, Color color) {
 		super(board, color);
 	}
+	
+	@Override
+	public String toString() {
+		return "P";
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
@@ -58,10 +63,5 @@ public class Pawn extends ChessPiece {
 		
 		return mat;
 	}
-	
-	@Override
-	public String toString() {
-		return "P";
-	}
-	
+		
 }
