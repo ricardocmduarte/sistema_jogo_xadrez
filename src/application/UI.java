821106df38d93dddf8bc3fec83b2ed9547c5e6d5
Ricardo Duarte <ricnewline@gmail.com>
2020-09-03 20:52:05 -0300

    Ignore Eclipse folder

--- src/application/UI.java
+++ src/application/UI.java
@@ -81,7 +81,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("    a          b          c          d          e          f          g          h");
+		System.out.println("   a  b  c  d  e  f  g  h");
 
 	}
 	
@@ -95,7 +95,7 @@ public class UI {
 			}
 			System.out.println();
 		}
-		System.out.println("    a          b          c          d          e          f          g          h");
+		System.out.println("   a  b  c  d  e  f  g  h");
 		
 	}
 
@@ -124,11 +124,11 @@ public class UI {
 		System.out.print("White: ");
 		System.out.print(ANSI_WHITE);
 		System.out.println(Arrays.toString(white.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 		System.out.print("Black: ");
 		System.out.print(ANSI_YELLOW);
 		System.out.println(Arrays.toString(black.toArray()));
-		System.out.println(ANSI_RESET);
+		System.out.print(ANSI_RESET);
 	}
 
 }
